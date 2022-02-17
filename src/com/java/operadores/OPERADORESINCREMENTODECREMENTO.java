7b0ad35e5a0c77cdfe3da90933c2c8656405b754
perkles <maikosilvae@gmail.com>
2022-02-17 15:54:21 -0300

    cap2aula10

---
+++ src/com/java/operadores/OPERADORESINCREMENTODECREMENTO.java
@@ -0,0 +1,38 @@
+package com.java.operadores;
+
+public class OPERADORESINCREMENTODECREMENTO {
+	
+	public static void main(String[] args) {
+	
+		int x = 10;
+		
+		x++;
+		System.out.println("x = " + x);
+		
+		x--;
+		System.out.println("x = " + x);
+		
+		
+		int y = 10;
+		
+		++y;
+		System.out.println("y = " + y);
+		
+		--y;
+		System.out.println("y = " + y);
+		
+		
+		int z = 10, w = 5, a = 5;
+		
+		z = w++;
+		System.out.println("z = " + z);
+		System.out.println("w = " + w);
+		
+		z = --a;
+		System.out.println("z = " + z);
+		System.out.println("a = " + a);
+		
+	}
+	
+
+}
