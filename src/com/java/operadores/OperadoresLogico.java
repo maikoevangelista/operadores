beced87d71bb72c0f9960964c2b5c0031e2ea4b5
perkles <maikosilvae@gmail.com>
2022-03-09 07:45:26 -0300

    cap2aula12

---
+++ src/com/java/operadores/OperadoresLogico.java
@@ -0,0 +1,13 @@
+package com.java.operadores;
+
+public class OperadoresLogico {
+
+    public static void main(String[] args){
+
+        int idade = 20;
+
+        boolean precisaVotar = idade >= 18 && idade < 70;
+
+        System.out.println(precisaVotar);
+    }
+}
