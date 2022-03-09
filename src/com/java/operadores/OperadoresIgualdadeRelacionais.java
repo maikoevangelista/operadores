79903a9c5b2dd8ec2ad07778d9bf721df9db877d
perkles <maikosilvae@gmail.com>
2022-03-09 07:27:03 -0300

    cap2aula11

---
+++ src/com/java/operadores/OperadoresIgualdadeRelacionais.java
@@ -0,0 +1,15 @@
+package com.java.operadores;
+
+public class OperadoresIgualdadeRelacionais {
+
+	
+	public static void main (String[] args) {
+		
+		int idade = 20;
+		boolean maioridade = idade >= 18;
+		
+		System.out.println(maioridade);
+		
+		
+	}
+}
