6f9d945d62370b7bd3ae009f42a49fc28a51034c
perkles <maikosilvae@gmail.com>
2022-03-09 08:15:11 -0300

    cap2aula13

---
+++ src/com/java/operadores/OperadorLogico2.java
@@ -0,0 +1,13 @@
+package com.java.operadores;
+
+public class OperadorLogico2 {
+
+    public static void main(String[] args) {
+
+        int x = 9, y = 11;
+
+        boolean teste = x > 10 ^ y >10;
+
+        System.out.println();
+    }
+}
